b138f706cc0b17f2fc26fd955cdcfed3025a8769
Kaveendra Randeera <khkrandeera@gmail.com>
2024-01-31 12:13:44 +0530

    completed the body of the app

--- js/main.js
+++ js/main.js
@@ -3,33 +3,45 @@ const sub = document.getElementById("sub");
 const results = document.getElementById("results");
 
 sub.addEventListener("click", (e)=>{
-    e.preventDefault();
-    const listItem = document.createElement("li");
-    const addItem = document.createElement("span");
-    const deleteButton = document.createElement("button");
-    const doneButton = document.createElement("button");
 
-    addItem.classList.add("item");
-    addItem.innerText = inputElement.value;
+    if(inputElement.value.length > 0){
+        e.preventDefault();
+        const listItem = document.createElement("li");
+        const addItem = document.createElement("span");
+        const deleteButton = document.createElement("button");
+        const doneButton = document.createElement("button");
+    
+        addItem.classList.add("item");
+        addItem.innerText = inputElement.value;
+    
+        deleteButton.classList.add("del");
+        deleteButton.innerText = "Del";
+    
+        doneButton.classList.add("done");
+        doneButton.innerText = "Done";
+    
+        listItem.appendChild(addItem);
+        listItem.appendChild(doneButton);
+        listItem.appendChild(deleteButton);
+        results.appendChild(listItem);
+    
+        deleteButton.addEventListener("click", ()=> {
+            results.removeChild(listItem)
+        });
+    
+        doneButton.addEventListener("click", ()=> {
+            const span = listItem.querySelector(".item");
+            span.style.color = "#ADFF2F";
+            span.style.textDecoration = "line-through";
+        });
+    
+        inputElement.value = "";
+        inputElement.focus();
 
-    deleteButton.classList.add("del");
-    deleteButton.innerText = "Del";
+    }else{
+        e.preventDefault();
+    }
 
-    doneButton.classList.add("done");
-    doneButton.innerText = "Done";
-
-    listItem.appendChild(addItem);
-    listItem.appendChild(doneButton);
-    listItem.appendChild(deleteButton);
-    results.appendChild(listItem);
-
-    deleteButton.addEventListener("click", ()=> {
-        results.removeChild(listItem)
-    });
-
-    doneButton.addEventListener("click", ()=> {
-        alert("done");
-    });
 
 });
 
